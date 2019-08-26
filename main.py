3530e6fa4fe98dfd23ead545ffafff443d3b3c8e
Eleen Liu <eleenliu2@gmail.com>
2019-08-26 15:40:32 +1000

    renamed

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 """
 This project imports iMessage data from the MacBook archive from 2015-present;
 calculates volume totals, % of messages from sender and receiver, and topic modelling.
+
+** Note: This application is only compatible with MacBooks and iMacs **
 """
 
 import sqlite3
@@ -8,22 +10,23 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from wordcloud import WordCloud
 import seaborn as sns
+from datetime import datetime
 
-def style():
+
+def style(pal='husl'):            # stylize charts
     sns.set()
-    sns.set_palette('husl',10)
+    sns.set_palette(pal,10)
 
-def load_contacts():
-    # username = input("Input your username: ")
-    # conn = sqlite3.connect('/Users/', username, '/Library/Application Support/AddressBook/AddressBook-v22.abcddb')
 
+def load_contacts():        # load contacts from Mac database
     conn = sqlite3.connect('/Users/Eleen/Library/Application Support/AddressBook/AddressBook-v22.abcddb')
     contacts_query = "SELECT DISTINCT ZFIRSTNAME || ' ' || ZLASTNAME AS FULLNAME, ZFULLNUMBER FROM ZABCDPHONENUMBER " \
                      "LEFT JOIN ZABCDRECORD ON ZABCDRECORD.Z_PK = ZABCDPHONENUMBER.ZOWNER"
     contacts = pd.read_sql_query(contacts_query, conn)
 
     for count in range(0,len(contacts['ZFULLNUMBER'])):
-        contacts['ZFULLNUMBER'][count] = contacts['ZFULLNUMBER'][count].replace(" ", "").replace(" ","").replace("(", "").replace(")", "").replace("-","")
+        contacts['ZFULLNUMBER'][count] = contacts['ZFULLNUMBER'][count]\
+            .replace(" ", "").replace(" ", "").replace("(", "").replace(")", "").replace("-", "")
         string = contacts['ZFULLNUMBER'][count]
         if string[:1] != "+":
             contacts['ZFULLNUMBER'][count] = "+1" + contacts['ZFULLNUMBER'][count]
@@ -31,9 +34,6 @@ def load_contacts():
 
 
 def get_df():    # creates data frame from local iMessage database
-    # username = input("Input your username: ")
-    # conn = sqlite3.connect('/Users/', username, '/Library/Messages/chat.db')
-
     conn = sqlite3.connect('/Users/Eleen/Library/Messages/chat.db')
     message_query = " SELECT *, datetime(message.date + strftime('%s', '2001-01-01') ,'unixepoch','localtime') " \
                     "as date_uct  FROM message "
@@ -49,7 +49,7 @@ def get_df():    # creates data frame from local iMessage database
     return merge_df
 
 
-def get_info():   # get phone number and name
+def get_info():   # get phone number and name from user
     is_default = input("Press X for default value, Y to input from contacts, or any other button to continue: ")
 
     if is_default.upper() == "X":
@@ -78,7 +78,7 @@ def get_info():   # get phone number and name
     return num_list, name_str
 
 
-def volume(df):
+def volume(df):     # show total iMessage volume
     msg_from_me = df[df['is_from_me'] == 1]
     print("Total Messages:", len(df))
     print("Messages from Me to Recipient: ", len(msg_from_me))
@@ -102,30 +102,26 @@ def word_cloud(df):      # create word cloud of iMessage history
         for x in df['text']:
             x += " "
             text += x
-    except TypeError:
+    except TypeError:           # pass by NoneTypes
         pass
 
     try:
-        cloud = WordCloud(max_words=60, background_color="white", width=1600, height=800).generate(text)
+        cloud = WordCloud(max_words=60, background_color="white", width=1600, height=800, stopwords=None).generate(text)
         plt.figure(figsize=(20, 10), facecolor='k')
         plt.imshow(cloud)
         plt.show()
-    except ValueError:
+    except ValueError:                  # if there is no data
         print("Word cloud cannot be generated.")
 
 
-def time_series(df):
+def time_series(df):        # charts frequency of iMessages by month
+    print("Generating time series by month...")
     style()
     df["date_uct"] = df["date_uct"].astype("datetime64")
     month = df['date_uct'].dt.month
-    year = df['date_uct'].dt.year
-    sns.countplot(x=month, data=df)
+    plot = sns.countplot(x=month, data=df)
+    plot.set(xlabel='Month', ylabel='Count', title='Frequency of iMessages by Month')
     plt.show()
-
-
-def show_text(df):
-    for msg in df['text']:
-        print(msg)
 
 
 pd.options.mode.chained_assignment = None
@@ -139,6 +135,6 @@ received_msgs = target_df[target_df['is_from_me'] != 1]     # filter messages no
 print("\nShowing iMessage stats for", name, "...\n")
 volume(target_df)
 percent(target_df)
-# show_text(target_df)
-# word_cloud(target_df)
+word_cloud(target_df)
 time_series(target_df)
+
